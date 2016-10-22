abd432f6f0b5a15e9c120d173f1fdb4eebc78bac
Paul Tagliamonte <tag@pault.ag>
2016-10-21 23:13:32 -0400

    fix some broken nonsense

--- cmd/render/forecast.go
+++ cmd/render/forecast.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	forecast "github.com/mlbright/forecast/v2"
+)
+
+type ForecastData struct {
+	Forecast forecast.Forecast
+}
+
+type Forecast struct {
+	lat    string
+	lon    string
+	apikey string
+}
+
+func (f Forecast) Config() CardConfig {
+	return CardConfig{Template: "weather"}
+}
+
+func (f Forecast) Query() (interface{}, error) {
+	data, err := forecast.Get(f.apikey, f.lat, f.lon, "now", forecast.US)
+	return &ForecastData{
+		Forecast: *data,
+	}, err
+}
+
+func NewForecast(apikey, lat, lon string) (*Forecast, error) {
+	return &Forecast{
+		lat:    lat,
+		lon:    lon,
+		apikey: apikey,
+	}, nil
+}
--- cmd/render/server.go
+++ cmd/render/server.go
@@ -1,15 +1,87 @@
 package main
 
 import (
-	// "html/template"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"html/template"
 	"net/http"
 )
 
+func loadTemplates(root string) (*template.Template, error) {
+	files := []string{}
+	filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		if !strings.HasSuffix(path, ".html") {
+			return nil
+		}
+		files = append(files, path)
+		return nil
+	})
+	return template.ParseFiles(files...)
+}
+
+type Server struct {
+	cards []Card
+}
+
+func (s *Server) Add(c Card) {
+	s.cards = append(s.cards, c)
+}
+
+func (s *Server) Query() (map[string]interface{}, error) {
+	query := map[string]interface{}{}
+	for _, card := range s.cards {
+		config := card.Config()
+		data, err := card.Query()
+		if err != nil {
+			return nil, err
+		}
+		query[config.Template] = data
+	}
+
+	return query, nil
+}
+
+func NewServer() (*Server, error) {
+	return &Server{
+		cards: []Card{},
+	}, nil
+}
+
 func main() {
+	forecast, _ := NewForecast(
+		"ec481833289cd59606c38a87a6a408f0",
+		"38.874609",
+		"-77.001758",
+	)
+
+	server, err := NewServer()
+	if err != nil {
+		panic(err)
+	}
+	server.Add(forecast)
+
 	fs := http.FileServer(http.Dir("output"))
 	http.Handle("/output/", http.StripPrefix("/output/", fs))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Foo!\n"))
+		template, err := loadTemplates("templates")
+		if err != nil {
+			log.Fatalf("%s\n", err)
+			return
+		}
+		query, err := server.Query()
+		if err != nil {
+			log.Fatalf("%s\n", err)
+			return
+		}
+		w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+		w.WriteHeader(200)
+		template.ExecuteTemplate(w, "index.html", query)
 	})
 	http.ListenAndServe(":8080", nil)
 }
